80ff7fb771afb9edcaa8e65791d7580a6e6a604b
ashishbct <ashish.vishwakarma@gravityilabs.com>
2024-06-08 13:54:49 +0530

    gif

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Random-GIF
-Tech :  React
+# random-gifs
---
+++ src/components/Tag.jsx
@@ -0,0 +1,10 @@
+export default function Tag() {
+  return (
+    <div className="bg-green-400 w-96 h-96 mt-2">
+         <div className="flex flex-col items-center bg-black text-white">
+
+        <h1>Tag</h1>
+      </div>
+    </div>
+  );
+}
